c765aa737e84db33d717012d6a377ebbe753e4e0
Mushieh Edison <mushiehedison66@gmail.com>
2025-07-24 22:49:46 +0100

    new message

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server:{
+        proxy:{
+            '/api':'https://backend-b5jw.onrender.com',
+        }
+    },
   plugins: [react()],
 })
